2399c3de2d614803e089800d3fe536508bef3c79
Rizal Fadlullah <izzal@Rizals-MacBook-Air.local>
2025-03-25 08:10:36 +0700

    Initial commit

---
+++ Brewfile
@@ -0,0 +1,8 @@
+brew 'git'
+brew 'nvim'
+brew 'gh'
+cask 'ghostty'
+cask 'microsoft-word'
+cask 'microsoft-excel'
+cask 'microsoft-powerpoint'
+
---
+++ install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+setup() {
+  set -e
+  caffeinate &
+
+  TMPFILE=$(mktemp)
+  trap 'echo "Cleaning up credentioals..."; rm -f "$TMPFILE"' EXIT
+  
+  for i in 1 2 3; do
+    read -s -p "Password: " PASSWORD
+    echo
+    
+    # Check if the password is correct by testing with sudo -v
+    echo "$PASSWORD" | sudo -S -v &>/dev/null
+    if [[ $? -eq 0 ]]; then
+      # Create the temporary script with the correct password
+      printf "#!/bin/bash\necho \"%s\"\n" "$PASSWORD" > "$TMPFILE"
+      chmod +x "$TMPFILE"
+      export SUDO_ASKPASS="$TMPFILE"
+      return
+    fi
+
+    echo "Sorry, try again."
+  done
+  
+  # If all attempts fail, exit the script
+  echo "sudo: 3 incorrect password attempts"
+  exit 1
+}
+
+main(){
+  setup
+
+  BREW_SHELLENV="/opt/homebrew/bin/brew shellenv"
+  BREW_BIN="/opt/homebrew/bin/brew"
+
+  # Check if Homebrew was installed
+  if [ ! -x "$BREW_BIN" ]; then
+    # Install Homebrew
+    NONINTERACTIVE=1 \
+    /bin/bash -c "$(curl -fsSL \
+    https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # Add Homebrew to .zprofile if not already present
+    grep -Fxq "eval \"\$($BREW_SHELLENV)\"" ~/.zprofile || \
+      echo "eval \"\$($BREW_SHELLENV)\"" | tee -a ~/.zprofile
+
+    # Evaluate Homebrew's shell environment
+    eval "$($BREW_SHELLENV)"
+    echo "Homebrew installed and configured successfully."
+  fi
+
+  # Run Brew bundle
+  brew bundle --verbose
+}
+
+main
