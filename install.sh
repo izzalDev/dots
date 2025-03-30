6371ce52203eed9c1b6beb9bba07820a0592ed3a
Rizal Fadlullah <izzal@Rizals-MacBook-Air.local>
2025-03-30 11:41:21 +0700

    chore: refactoring install.sh and add postinstall script for ghostty

--- install.sh
+++ install.sh
@@ -1,36 +1,70 @@
 #!/bin/bash
 
-main(){
-  mkdir -p ~/Repositories ~/Obsidian\ Vault/
-  echo A | softwareupdate --install-rosetta
+function main {
   setup_sudo_askpass
+  mkdir -p ~/Repositories ~/Obsidian\ Vault/
+  install_rosetta2
   setup_homebrew
   brew bundle --verbose
   postinstall_xcode
+  postinstall_ghostty
   setup_dock
+  return 0
 }
 
-setup_sudo_askpass() {
-  set -e
-  caffeinate &
+function info {
+  local BLUE='\033[0;34m'
+  local NC='\033[0m'
+  printf "${BLUE}==>${NC} ${@}\n" 
+}
 
-  TMPFILE=$(mktemp)
-  trap 'echo "Cleaning up credentials..."; rm -f "$TMPFILE"' EXIT
+function success {
+  local GREEN='\e[0;33m'
+  local NC='\033[0m'
+  printf "${GREEN}==>${NC} ${@}\n"
+}
+
+function install_rosetta2 {
+  if /usr/bin/arch -x86_64 /bin/true 2>/dev/null; then
+    info "Rosetta 2 is already installed. No action needed."
+    exit 0
+  fi
+
+  info "Rosetta 2 is not installed. Installing now..."
+  echo "A" | sudo -A softwareupdate --install-rosetta --agree-to-license
+}
+
+function postinstall_ghostty {
+  if ! brew list --cask | grep -q "ghostty"; then
+    return 0
+  fi
+
+  info "Executing postinstall script for ghostty..."
+
+  local CONFIG_DIR="${HOME}/.config/ghostty"
+  local CONFIG_FILE="${HOME}/.config/ghostty/config"
+  mkdir -p "${CONFIG_DIR}"
+  echo "macos-titlebar-style = hidden" > "${CONFIG_FILE}"
+}
+
+function setup_sudo_askpass {
+  local TMPFILE=$(mktemp)
   
-  for i in 1 2 3; do
+  trap 'echo "Cleaning up credentials..."; rm -f "${TMPFILE}"; kill ${CAFFEINE_PID} 2>/dev/null || true' EXIT
+  
+  for i in {1..3}; do
     read -s -p "Password: " PASSWORD
     echo
     
     # Check if the password is correct by testing with sudo -v
-    echo "$PASSWORD" | sudo -S -v &>/dev/null
+    echo "${PASSWORD}" | sudo -S -v &>/dev/null
     if [[ $? -eq 0 ]]; then
       # Create the temporary script with the correct password
-      printf "#!/bin/bash\necho \"%s\"\n" "$PASSWORD" > "$TMPFILE"
-      chmod +x "$TMPFILE"
-      export SUDO_ASKPASS="$TMPFILE"
+      printf "#!/bin/zsh\necho \"%s\"\n" "${PASSWORD}" > "${TMPFILE}"
+      chmod +x "${TMPFILE}"
+      export SUDO_ASKPASS="${TMPFILE}"
       return
     fi
-
     echo "Sorry, try again."
   done
   
@@ -39,50 +73,35 @@ setup_sudo_askpass() {
   exit 1
 }
 
-setup_homebrew(){
-  BREW_SHELLENV="/opt/homebrew/bin/brew shellenv"
-  BREW_BIN="/opt/homebrew/bin/brew"
-
-  # Check if Homebrew was installed
-  if [ ! -x "$BREW_BIN" ]; then
-    # Install Homebrew
+function setup_homebrew {
+  local BREW_SHELLENV="/opt/homebrew/bin/brew shellenv"
+  local BREW_BIN="/opt/homebrew/bin/brew"
+  if [[ ! -x "${BREW_BIN}" ]]; then
     NONINTERACTIVE=1 \
-    /bin/bash -c "$(curl -fsSL \
+    /bin/zsh -c "$(curl -fsSL \
     https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-    # Add Homebrew to .zprofile if not already present
-    grep -Fxq "eval \"\$($BREW_SHELLENV)\"" ~/.zprofile || \
-      echo "eval \"\$($BREW_SHELLENV)\"" | tee -a ~/.zprofile
-
-    # Evaluate Homebrew's shell environment
-    eval "$($BREW_SHELLENV)"
-    echo "Homebrew installed and configured successfully."
+    grep -Fxq "eval \"\$(${BREW_SHELLENV})\"" ~/.zprofile || \
+      echo "eval \"\$(${BREW_SHELLENV})\"" | tee -a ~/.zprofile
+    eval "$(${BREW_SHELLENV})"
+    success "Homebrew installed and configured successfully."
   fi
 }
 
-setup_dock() {
-  BLUE='\033[0;34m'  # ANSI code for blue text
-  NC='\033[0m'       # Reset color
-
-  echo -e "${BLUE}==>${NC} Removing all dock items..."
+function setup_dock {
+  info "Removing all dock items..."
   defaults write com.apple.dock persistent-apps -array
-
-  echo -e "${BLUE}==>${NC} Enabling recent applications in Dock..."
+  info "Enabling recent applications in Dock..."
   defaults write com.apple.dock show-recents -bool true
-
-  echo -e "${BLUE}==>${NC} Setting recent applications count..."
+  info "Setting recent applications count..."
   defaults write com.apple.dock show-recent-count -int 10
-
-  echo -e "${BLUE}==>${NC} Restarting Dock..."
-  killall Dock
 }
 
-postinstall_xcode(){
-  if mas list | grep -q "Xcode"; then
+function postinstall_xcode {
+  if ! mas list | grep -q "Xcode"; then
     return 0
   fi
 
-  echo "==> Executing postinstall script for Xcode..."
+  info "Executing postinstall script for Xcode..."
   sudo -A xcode-select -s /Applications/Xcode.app/Contents/Developer
   sudo -A xcodebuild -license accept
   sudo -A xcodebuild -runFirstLaunch
@@ -90,3 +109,4 @@ postinstall_xcode(){
 }
 
 main
+
